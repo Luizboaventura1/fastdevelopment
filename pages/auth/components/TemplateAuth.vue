7b14deaa3076f09095043721300ab45b4815d7f4
Luiz Henrique <luizboaventura24@gmail.com>
2023-09-28 09:35:55 -0300

    Component do template para forms adicionado

---
+++ pages/auth/components/TemplateAuth.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="w-full h-screen bg-subSecondaryColorF">
+    <div class="container mx-auto px-3 h-full flex justify-center items-center">
+      <slot />
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
