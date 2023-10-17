316ba70141857092775536354837ca5f7a39e2e9
Luiz Henrique <luizboaventura24@gmail.com>
2023-10-17 11:04:25 -0300

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
