200e82767ba0a867e1a48f247d03af6a45d531fe
Luiz Henrique <luizboaventura24@gmail.com>
2023-10-28 12:07:33 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,9 +15,8 @@ export default defineNuxtConfig({
   ],
   pinia: {
     autoImports: [
-      // automatically imports `defineStore`
-      'defineStore', // import { defineStore } from 'pinia'
-      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+      'defineStore',
+      ['defineStore', 'definePiniaStore'],
     ],
   },
   runtimeConfig: {
