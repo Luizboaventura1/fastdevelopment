48e79c20cd1b577bd38255d1501b074c37815df3
Luiz Henrique <luizboaventura24@gmail.com>
2023-11-19 16:03:44 -0300

    Novo component

---
+++ pages/dashboard/components/WorkspaceComponents/FrameBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    :class="`my-${props.spaceY} mx-${props.spaceX}`"
+    class="flex justify-between rounded-full bg-secondaryColorF px-4 py-3 ring-1 ring-zinc-700"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+let props = defineProps({
+  spaceY: String,
+  spaceX: String,
+});
+</script>
