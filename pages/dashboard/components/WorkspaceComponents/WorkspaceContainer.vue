cd647067ddec28bf0cf191ac21c3b09823a9f938
Luiz Henrique <luizboaventura24@gmail.com>
2023-11-19 16:03:26 -0300

    Novo component

---
+++ pages/dashboard/components/WorkspaceComponents/WorkspaceContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="`p-${padding}`">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+let props = defineProps({
+  sm: {
+    type: Boolean,
+    default: false,
+  },
+  md: {
+    type: Boolean,
+    default: false,
+  },
+  lg: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const padding = computed(() => {
+  if (props.sm) return "4";
+  else if (props.md) return "6";
+  else if (props.lg) return "8";
+});
+</script>
+
+<style lang="scss" scoped></style>
