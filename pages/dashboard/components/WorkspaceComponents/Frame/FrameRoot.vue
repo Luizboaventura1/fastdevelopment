39b782a540318ac1d9f685c51b80bef0bc035036
Luiz Henrique <luizboaventura24@gmail.com>
2023-11-19 16:03:48 -0300

    Novo component

---
+++ pages/dashboard/components/WorkspaceComponents/Frame/FrameRoot.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link :to="`/dashboard/${frameID}`">
+    <div class="frame cursor-pointer w-full h-[200px] ring-1 ring-zinc-700 hover:ring-zinc-500 transition-all duration-300 rounded-lg p-4">
+      <slot />
+    </div>
+  </nuxt-link>
+</template>
+
+<script setup>
+let route = useRoute()
+defineProps({
+  frameID: String
+})
+</script>
+
+<style lang="scss" scoped>
+.frame {
+  background-color: #18181a;
+}
+</style>
