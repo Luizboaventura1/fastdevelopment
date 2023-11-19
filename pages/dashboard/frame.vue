36c0d73f46f0327185632ab8e7727c5b1ab0f7fa
Luiz Henrique <luizboaventura24@gmail.com>
2023-11-19 16:03:16 -0300

    Página removida

--- pages/dashboard/frame.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="w-full min-w-max h-full">
-    <KanbanRoot />
-  </div>
-</template>
-
-<script setup>
-import KanbanRoot from '~/components/Kanban/KanbanRoot.vue';
-</script>
