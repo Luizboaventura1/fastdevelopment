53dcf829bb04651c9dd16370b52f92f88a56763d
Luiz Henrique <luizboaventura24@gmail.com>
2023-11-19 16:04:03 -0300

    Novo component

---
+++ pages/dashboard/components/WorkspaceComponents/CreateNewFrame/CreateNewFrameRoot.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="frame w-full h-[200px] ring-1 ring-zinc-700 hover:ring-zinc-500 transition-all duration-300 rounded-lg p-4">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+let route = useRoute()
+
+</script>
+
+<style lang="scss" scoped>
+.frame {
+  background-color: #18181a;
+}
+</style>
