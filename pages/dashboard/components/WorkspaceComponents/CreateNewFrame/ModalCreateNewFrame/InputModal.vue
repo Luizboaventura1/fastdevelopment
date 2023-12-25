8c04be80fb2d981fb6e7fe3d1e69da4d0a163eae
Luiz Henrique <luizboaventura24@gmail.com>
2023-12-25 16:52:44 -0300

    Update InputModal.vue

--- pages/dashboard/components/WorkspaceComponents/CreateNewFrame/ModalCreateNewFrame/InputModal.vue
+++ pages/dashboard/components/WorkspaceComponents/CreateNewFrame/ModalCreateNewFrame/InputModal.vue
@@ -6,6 +6,7 @@
     class="w-full border-0 outline-0 text-white rounded-lg bg-secondaryColorF p-3 ring-2 ring-transparent focus:ring-primaryColorF"
     type="text"
     :placeholder="props.placeholderInput"
+    autocomplete="off"
   >
 </template>
 
