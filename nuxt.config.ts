f48b13c2ccc644f5271a98f59c1cb676404249d3
Luiz Henrique <luizboaventura24@gmail.com>
2024-10-16 10:28:43 -0300

    version update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,30 +2,38 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   imports: {
     autoImport: true
   },
+
   modules: [
     '@pinia/nuxt',
     '@vueuse/motion/nuxt',
     'nuxt-gtag',
   ],
+
   plugins: [
     "~/plugins/firebase.js",
   ],
+
   pinia: {
     autoImports: [
       'defineStore',
       ['defineStore', 'definePiniaStore'],
     ],
   },
+
   gtag: {
     id: 'G-1P1CHBPTH6'
-  }
+  },
+
+  compatibilityDate: '2024-10-16'
 })
