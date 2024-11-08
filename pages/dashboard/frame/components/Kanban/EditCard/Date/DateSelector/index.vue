976d099449b2649de5e1d96f62d27e379d5179d0
Luiz Henrique <luizboaventura24@gmail.com>
2024-11-08 20:14:00 -0300

    Delete unnecessary subfolder

--- pages/dashboard/frame/components/Kanban/EditCard/Date/DateSelector/index.vue
+++ pages/dashboard/frame/components/Kanban/EditCard/Date/DateSelector/index.vue
@@ -34,7 +34,7 @@ import CloseButton from "~/components/Common/FeedBack/CloseButton.vue";
 import PrimaryButton from "~/components/Common/Buttons/PrimaryButton.vue";
 import ClearButton from "./ClearButton.vue";
 import ModalBackdrop from "./ModalBackdrop.vue";
-import Calendar from "../Calendar/index.vue";
+import Calendar from "../Calendar";
 import CheckBoxButton from "./CheckBoxButton.vue";
 import { useWorkspace } from "~/stores/workspace";
 import type Frame from "~/stores/types/frame";
